c904bacf0398ec274cc7d9ba20aa39a5b2ddfc6f
Gabriel Mascarenhas <gabrielmrdp@gmail.com>
2021-01-08 21:18:36 -0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -22,7 +22,7 @@ SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
 # Server-side Parameters
 CLIENT_SIDE_URL = "https://spotify-dragdrop.herokuapp.com/"
 PORT = 5000
-REDIRECT_URI = "{}:{}/callback/".format(CLIENT_SIDE_URL, PORT)
+REDIRECT_URI = "{}:{}/callback/"#.format(CLIENT_SIDE_URL, PORT)
 SCOPE = "playlist-modify-public playlist-modify-private"
 STATE = ""
 SHOW_DIALOG_bool = True
